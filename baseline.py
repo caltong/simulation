43bf26e16287021e45322795d6a6a68db63277b6
caltong <caltong1027@gmail.com>
2019-10-26 23:16:21 +0800

    添加实验数据做评估

--- baseline.py
+++ baseline.py
@@ -1,4 +1,4 @@
-from load_dataset import load_dataset
+from load_dataset import load_dataset, load_test_dataset
 from torch.distributions.one_hot_categorical import OneHotCategorical
 import torch
 from torch import nn
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 
 use_gpu = torch.cuda.is_available()
-num_epochs = 5000
+num_epochs = 500
 batch_size = 40
 learning_rate = 0.05
 
@@ -33,6 +33,13 @@ class SimulationDataset(Dataset):
         return sample
 
 
+class TestSimulationDataset(SimulationDataset):
+    def __init__(self, transform=None):
+        super().__init__(transform)
+        self.all_inputs, self.all_outputs = load_test_dataset()
+        self.transform = transform
+
+
 class ToTensor(object):
     def __call__(self, sample):
         inputs, outputs = sample['input'], sample['output']
@@ -40,29 +47,33 @@ class ToTensor(object):
 
 
 simulation_dataset = SimulationDataset(transform=transforms.Compose([ToTensor()]))
+test_dataset = TestSimulationDataset(transform=transforms.Compose([ToTensor()]))
 # for i in range(5):
-#     sample = simulation_dataset[i]
+#     sample = test_dataset[i]
 #     print(sample['input'])
 #     print(sample['output'])
 
 data_loader = DataLoader(simulation_dataset, batch_size=batch_size, shuffle=True)
+test_data_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 
 class BaselineModel(nn.Module):
-    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, n_hidden_3, out_dim):
         super(BaselineModel, self).__init__()
         self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
         self.layer2 = nn.Sequential(nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
-        self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, out_dim), nn.ReLU(True))
+        self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, n_hidden_3), nn.ReLU(True))
+        self.layer4 = nn.Sequential(nn.Linear(n_hidden_3, out_dim), nn.ReLU(True))
 
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.layer4(x)
         return x
 
 
-model = BaselineModel(1280, 256, 256, 10)
+model = BaselineModel(1280, 1024, 256, 256, 10)
 # print(model)
 if use_gpu:
     model = model.cuda()
@@ -94,3 +105,18 @@ for epoch in range(num_epochs):
         if i % 300 == 0:
             print(f'[{epoch + 1}/{num_epochs}] Loss: {running_loss / i:.6f}, Acc: {running_acc / i:.6f}')
     print(f'Finish {epoch + 1} epoch, Loss: {running_loss / i:.6f}, Acc: {running_acc / i:.6f}')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in test_data_loader:
+        x_train, y_train = data['input'], data['output']
+        if use_gpu:
+            x_train = x_train.cuda()
+            y_train = y_train.cuda()
+        output = model(x_train)
+        _, pred = torch.max(output.data, 1)
+        total += y_train.size(0)
+        correct += (pred == y_train).sum().item()
+print('Accuracy of the network on the 12 test data: %d %%' % (
+        100 * correct / total))
--- load_dataset.py
+++ load_dataset.py
@@ -10,6 +10,21 @@ def get_one_file_data(file_path):
     return one_file_data
 
 
+def get_one_file_data_and_compress(file_path):
+    one_file_data = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            one_file_data.append(float(line))
+        length = len(one_file_data)
+        rate = int(length / 128)
+        compressed_one_file_data = []
+        for i in range(128):
+            s = sum(one_file_data[i * rate:i * rate + 128]) / rate
+            compressed_one_file_data.append(s)
+
+    return compressed_one_file_data
+
+
 def load_dataset():
     x = []
     y = []
@@ -22,6 +37,24 @@ def load_dataset():
         data_single = []
         for file in files:
             data_single.append(get_one_file_data(os.path.join(subdir, file)))
+        x.append(data_single)
+    x = np.array(x).reshape((-1, 1280))
+    y = np.array(y)
+    return x, y
+
+
+def load_test_dataset():
+    x = []
+    y = []
+    path = os.path.join('experiment')
+    dirs = os.listdir(path)
+    for subdir in dirs:
+        y.append(int(subdir[-1]) - 1)  # 1-10 to 0-9
+        subdir = os.path.join(path, subdir)
+        files = os.listdir(subdir)
+        data_single = []
+        for file in files:
+            data_single.append(get_one_file_data_and_compress(os.path.join(subdir, file)))
         x.append(data_single)
     x = np.array(x).reshape((-1, 1280))
     y = np.array(y)
